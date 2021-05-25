2cc3761a7bf3adf38e8120f98bd45a7489951cb6
lainasuarez <lainasuarez@gmail.com>
2021-05-24 21:24:53 -0500

    checking in my hw04 files

--- hw04/your_task/js/scripts.js
+++ hw04/your_task/js/scripts.js
@@ -17,28 +17,84 @@ const search = (ev) => {
 }
 
 const getTracks = (term) => {
-    console.log(`
-        get tracks from spotify based on the search term
-        "${term}" and load them into the #tracks section 
-        of the DOM...`);
+    url = baseURL + "?type=track&q=" + term + "&limit=5";
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#tracks').innerHTML = '';
+            if (data.length > 0) {
+                for (const track of data) {
+                    const template = `<section class="track-item preview" data-preview-track="${track.preview_url}">
+                        <img src="${track.album.image_url}">
+                        <i class="fas play-track fa-play" aria-hidden="true"></i>
+                        <div class="label">
+                            <h3>${track.name}</h3>
+                            <p>
+                                ${track.artist.name}
+                            </p>
+                        </div>
+                    </section>`;
+                    document.querySelector('#tracks').innerHTML += template;
+                    for (const elem of document.querySelectorAll('.track-item.preview')){
+                        elem.onclick = playTrack;
+                    }     
+                } 
+            } else {
+                document.querySelector('#tracks').innerHTML = 'No results to display, try searching for something else.';
+            }  
+        })
 };
 
+const playTrack = (ev) => {
+    console.log(ev.currentTarget);
+    const elem = ev.currentTarget;
+    const previewURL = elem.getAttribute('data-preview-track');
+    if (previewURL) {
+        audioPlayer.setAudioFile(previewURL);
+        audioPlayer.play();
+
+    } else {
+        console.log('there is no preview available for this track.')
+    }
+}
+
 const getAlbums = (term) => {
-    console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
+    url = baseURL + "?type=album&q=" + term;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#albums').innerHTML = '';
+            if (data.length > 0){
+                for (const album of data) {
+                    const template = `<section class="album-card" id=${album.id}>
+                    <div>
+                        <img src=${album.image_url}>
+                        <h3>${album.name}</h3>
+                        <div class="footer">
+                            <a href=${album.spotify_url} target="_blank">
+                                view on spotify
+                            </a>
+                        </div>
+                    </div>
+                </section>`;
+                document.querySelector('#albums').innerHTML += template; 
+                }
+            } else { document.querySelector('#albums').innerHTML = 'No results to display, try searching for something else.';
+            }
+         })
 };
 
 const getArtist = (term) => {
    const elem = document.querySelector('#artist');
-   elem.innerHTML = "";
+   elem.innerHTML = '';
    fetch(baseURL + '?type=artist&q=' + term)
    .then(response => response.json())
    .then((data) => {
        if (data.length > 0) {
            const firstArtist = data[0];
            elem.innerHTML += getArtistHTML(firstArtist);
+       } else {
+           elem.innerHTML = 'No results to display, try searching for something else.';
        }
    });
 };
